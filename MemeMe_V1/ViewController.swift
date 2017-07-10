76830461b703a13ad8bb2c2e3212b7d1ceb78e6c
Tim Spears <tmspears@gmail.com>
2017-07-10 12:01:43 -0400

    Initial Commit

---
+++ MemeMe_V1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe_V1
+//
+//  Created by Timothy Spears on 7/10/17.
+//  Copyright © 2017 Timothy Spears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
