689f3af1c8b721925aae2b83b3ea2232e1265445
Tim Spears <tmspears@gmail.com>
2017-07-10 13:04:44 -0400

    Setup initial UI with constraints, outlets, and actions.

--- MemeMe_V1/ViewController.swift
+++ MemeMe_V1/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func selectMemeImage(_ sender: Any) {
+    }
+    
+    @IBAction func useCameraForImage(_ sender: Any) {
+    }
 
 }
 
