6a8615ef7f4bb98f3d5f8fdb4a355abc26da9137
Tim Spears <tmspears@gmail.com>
2017-07-18 17:45:07 -0400

    rename main viewcontroller

--- MemeMe_V1/MemeEditorViewController.swift
+++ MemeMe_V1/MemeEditorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     // IB Outlets
     @IBOutlet weak var cameraButton: UIBarButtonItem!
