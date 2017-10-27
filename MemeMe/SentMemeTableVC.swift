6e8415ece38e1c8224f1623326f676c2a375ddee
Tim Spears <tmspears@gmail.com>
2017-10-27 17:55:16 -0400

    Added table protocol reqs

--- MemeMe/SentMemeTableVC.swift
+++ MemeMe/SentMemeTableVC.swift
@@ -10,7 +10,7 @@ import Foundation
 
 import UIKit
 
-class SentMemeTableVC: UIViewController {
+class SentMemeTableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // MARK - Properties
     var memes: [Meme]!
     
@@ -19,6 +19,12 @@ class SentMemeTableVC: UIViewController {
     
     // MARK - Lifecycle
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let sharedModel = UIApplication.shared.delegate as! AppDelegate
+        memes = sharedModel.memes
+    }
+    
     // MARK - IB Actions
     @IBAction func addMemeButton(_ sender: UIBarButtonItem) {
         self.performSegue(withIdentifier: "AddMemeSegue", sender: self)
@@ -26,5 +32,17 @@ class SentMemeTableVC: UIViewController {
     
     // MARK - Table Delegate
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let meme = self.memes[(indexPath as NSIndexPath).row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCell", for: indexPath)
+
+        cell.imageView?.image = meme.savedMeme
+        cell.textLabel?.text = meme.topText + " " + meme.bottomText
+        
+        return cell
+    }
 }
