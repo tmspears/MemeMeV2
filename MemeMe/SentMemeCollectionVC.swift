760d667fdcd540a05da657f76985cf936c87966a
Tim Spears <tmspears@gmail.com>
2017-10-27 14:01:35 -0400

    Add tab, nav, table, and collection views and VC

---
+++ MemeMe/SentMemeCollectionVC.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemeCollectionVC.swift
+//  MemeMe
+//
+//  Created by Timothy Spears on 10/27/17.
+//  Copyright © 2017 Timothy Spears. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class SentMemeCollectionVC {
+    
+}
